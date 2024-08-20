0feb0e477b08b02b7d2cfe2eeb5a91114260c597
Shahid7424 <shahshahid121212@gmail.com>
2024-08-20 06:57:19 -0700

    add lader

--- src/app/layoutComponent/loader.tsx
+++ src/app/layoutComponent/loader.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+"use client";
+import React, { useState, useEffect } from 'react';
+import Image from 'next/image';
+import { LoaderImage } from '../utils/images';
 
 const Loader = () => {
   return (
-    <div>Loading...</div>
-  )
-}
+    <div className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Image src={LoaderImage} alt="Loading..." />
+    </div>
+  );
+};
 
-export default Loader
+export default Loader;
